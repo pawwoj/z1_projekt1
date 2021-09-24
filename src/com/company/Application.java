204c562395e411b195103f0b8e6e490e14497485
PW <pwojdacki84@gmail.com>
2021-09-24 20:36:56 +0200

    Applications template

---
+++ src/com/company/Application.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Application {
+    public static void run(String operation, String a, String b) {
+        System.out.println("Operation: " + operation + " a:" + a + " b:" + b);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import static com.company.Application.run;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String operation = args[0];
+        String a = args[1];
+        String b = args[2];
+
+        run(operation, a, b);
+    }
+}
+
+// sout -> enter -> podpowiada System.out.println();
+// CTRL + Y -> usun linie
+// CTRL + ALT + SHIFT + L
+
+// ID: 1 NAME: STUDENT1
+// ID: 2 NAME: STUDENT2
+
+/*
+ * git clone link_do_projekt - klonowanie projektu
+ * cd - zmien sciezke
+ * ls - wypisz zawartosc aktualnej sciezki
+ * clear - wyczysc ekran
+ * pwd - wyswietl aktualna sciezk
+ */
+/*
+* git status
+* git add pliki_do_dodania
+ */
